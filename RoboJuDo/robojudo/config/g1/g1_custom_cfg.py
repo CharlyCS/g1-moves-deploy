4a310397038b6f6aa54a36e71ab16db68f2595ad
CharlyCS <ccoronels@uni.pe>
2026-08-05 18:05:01 -0500

    add dances

--- RoboJuDo/robojudo/config/g1/g1_custom_cfg.py
+++ RoboJuDo/robojudo/config/g1/g1_custom_cfg.py
@@ -398,3 +398,548 @@ class g1_woah_real(G1RlLocoMimicPipelineCfg):
 
     do_safety_check: bool = True
 
+@cfg_registry.register
+class g1_steptouch_real(G1RlLocoMimicPipelineCfg):
+    """
+    B_DadDance con política de locomoción de respaldo.
+
+    Flujo:
+    1. Inicia usando locomoción estable.
+    2. START o R inicia el baile.
+    3. Al terminar el movimiento, vuelve automáticamente a locomoción.
+    4. El proceso continúa ejecutándose y mantiene al robot de pie.
+    """
+
+    dance_name: str = "J_Dance0_StepTouch"
+
+    robot: str = "g1"
+
+    env: G1RealEnvCfg = G1RealEnvCfg(
+        env_type="UnitreeCppEnv",
+
+        update_with_fk=True,
+        born_place_align=True,
+
+        unitree=G1UnitreeCfg(
+            net_if="eth0",
+        ),
+    )
+
+    ctrl: list[UnitreeCtrlCfg | KeyboardCtrlCfg] = [
+        UnitreeCtrlCfg(
+            combination_init_buttons=[],
+
+            triggers={
+                "A": "[SHUTDOWN]",
+                "Select": "[POLICY_LOCO]",
+                "Start": "[POLICY_MIMIC]",
+            },
+        ),
+
+        KeyboardCtrlCfg(
+            triggers={
+                "r": "[POLICY_MIMIC]",
+                "l": "[POLICY_LOCO]",
+                "o": "[SHUTDOWN]",
+            },
+        ),
+    ]
+
+    # Política que se encarga de mantener equilibrio y locomoción.
+    # Usa únicamente una política que ya hayas validado físicamente.
+    loco_policy: G1AmoPolicyCfg = G1AmoPolicyCfg(freq = 50) # la frecuencia se puede eliminar
+
+    mimic_policies: list[G1BeyondMimicPolicyCfg] = [
+        G1BeyondMimicPolicyCfg(
+            policy_name=dance_name,
+
+            ############
+            freq=50,
+            motion_fps=60.0,
+            playback_rate=1.0,
+
+            # Suavizado moderado.
+            action_beta=0.8,
+            ############
+
+            without_state_estimator=False,
+
+            override_robot_anchor_pos=False,
+
+            use_motion_from_model=True,
+
+            # Usa este campo solamente si el modelo no reporta
+            # correctamente el final del movimiento.
+            max_timestep=get_motion_frames(dance_name)
+        ),
+    ]
+
+    do_safety_check: bool = True
+
+@cfg_registry.register
+class g1_spiral_real(G1RlLocoMimicPipelineCfg):
+    """
+    B_DadDance con política de locomoción de respaldo.
+
+    Flujo:
+    1. Inicia usando locomoción estable.
+    2. START o R inicia el baile.
+    3. Al terminar el movimiento, vuelve automáticamente a locomoción.
+    4. El proceso continúa ejecutándose y mantiene al robot de pie.
+    """
+
+    dance_name: str = "B_SpiralDance"
+
+    robot: str = "g1"
+
+    env: G1RealEnvCfg = G1RealEnvCfg(
+        env_type="UnitreeCppEnv",
+
+        update_with_fk=True,
+        born_place_align=True,
+
+        unitree=G1UnitreeCfg(
+            net_if="eth0",
+        ),
+    )
+
+    ctrl: list[UnitreeCtrlCfg | KeyboardCtrlCfg] = [
+        UnitreeCtrlCfg(
+            combination_init_buttons=[],
+
+            triggers={
+                "A": "[SHUTDOWN]",
+                "Select": "[POLICY_LOCO]",
+                "Start": "[POLICY_MIMIC]",
+            },
+        ),
+
+        KeyboardCtrlCfg(
+            triggers={
+                "r": "[POLICY_MIMIC]",
+                "l": "[POLICY_LOCO]",
+                "o": "[SHUTDOWN]",
+            },
+        ),
+    ]
+
+    # Política que se encarga de mantener equilibrio y locomoción.
+    # Usa únicamente una política que ya hayas validado físicamente.
+    loco_policy: G1AmoPolicyCfg = G1AmoPolicyCfg(freq = 50) # la frecuencia se puede eliminar
+
+    mimic_policies: list[G1BeyondMimicPolicyCfg] = [
+        G1BeyondMimicPolicyCfg(
+            policy_name=dance_name,
+
+            ############
+            freq=50,
+            motion_fps=60.0,
+            playback_rate=1.0,
+
+            # Suavizado moderado.
+            action_beta=0.8,
+            ############
+
+            without_state_estimator=False,
+
+            override_robot_anchor_pos=False,
+
+            use_motion_from_model=True,
+
+            # Usa este campo solamente si el modelo no reporta
+            # correctamente el final del movimiento.
+            max_timestep=get_motion_frames(dance_name)
+        ),
+    ]
+
+    do_safety_check: bool = True
+
+@cfg_registry.register
+class g1_stretch_real(G1RlLocoMimicPipelineCfg):
+    """
+    B_DadDance con política de locomoción de respaldo.
+
+    Flujo:
+    1. Inicia usando locomoción estable.
+    2. START o R inicia el baile.
+    3. Al terminar el movimiento, vuelve automáticamente a locomoción.
+    4. El proceso continúa ejecutándose y mantiene al robot de pie.
+    """
+
+    dance_name: str = "B_StretchDance"
+
+    robot: str = "g1"
+
+    env: G1RealEnvCfg = G1RealEnvCfg(
+        env_type="UnitreeCppEnv",
+
+        update_with_fk=True,
+        born_place_align=True,
+
+        unitree=G1UnitreeCfg(
+            net_if="eth0",
+        ),
+    )
+
+    ctrl: list[UnitreeCtrlCfg | KeyboardCtrlCfg] = [
+        UnitreeCtrlCfg(
+            combination_init_buttons=[],
+
+            triggers={
+                "A": "[SHUTDOWN]",
+                "Select": "[POLICY_LOCO]",
+                "Start": "[POLICY_MIMIC]",
+            },
+        ),
+
+        KeyboardCtrlCfg(
+            triggers={
+                "r": "[POLICY_MIMIC]",
+                "l": "[POLICY_LOCO]",
+                "o": "[SHUTDOWN]",
+            },
+        ),
+    ]
+
+    # Política que se encarga de mantener equilibrio y locomoción.
+    # Usa únicamente una política que ya hayas validado físicamente.
+    loco_policy: G1AmoPolicyCfg = G1AmoPolicyCfg(freq = 50) # la frecuencia se puede eliminar
+
+    mimic_policies: list[G1BeyondMimicPolicyCfg] = [
+        G1BeyondMimicPolicyCfg(
+            policy_name=dance_name,
+
+            ############
+            freq=50,
+            motion_fps=60.0,
+            playback_rate=1.0,
+
+            # Suavizado moderado.
+            action_beta=0.8,
+            ############
+
+            without_state_estimator=False,
+
+            override_robot_anchor_pos=False,
+
+            use_motion_from_model=True,
+
+            # Usa este campo solamente si el modelo no reporta
+            # correctamente el final del movimiento.
+            max_timestep=get_motion_frames(dance_name)
+        ),
+    ]
+
+    do_safety_check: bool = True
+
+@cfg_registry.register
+class g1_broadway_real(G1RlLocoMimicPipelineCfg):
+    """
+    B_DadDance con política de locomoción de respaldo.
+
+    Flujo:
+    1. Inicia usando locomoción estable.
+    2. START o R inicia el baile.
+    3. Al terminar el movimiento, vuelve automáticamente a locomoción.
+    4. El proceso continúa ejecutándose y mantiene al robot de pie.
+    """
+
+    dance_name: str = "J_Dance4_Broadway"
+
+    robot: str = "g1"
+
+    env: G1RealEnvCfg = G1RealEnvCfg(
+        env_type="UnitreeCppEnv",
+
+        update_with_fk=True,
+        born_place_align=True,
+
+        unitree=G1UnitreeCfg(
+            net_if="eth0",
+        ),
+    )
+
+    ctrl: list[UnitreeCtrlCfg | KeyboardCtrlCfg] = [
+        UnitreeCtrlCfg(
+            combination_init_buttons=[],
+
+            triggers={
+                "A": "[SHUTDOWN]",
+                "Select": "[POLICY_LOCO]",
+                "Start": "[POLICY_MIMIC]",
+            },
+        ),
+
+        KeyboardCtrlCfg(
+            triggers={
+                "r": "[POLICY_MIMIC]",
+                "l": "[POLICY_LOCO]",
+                "o": "[SHUTDOWN]",
+            },
+        ),
+    ]
+
+    # Política que se encarga de mantener equilibrio y locomoción.
+    # Usa únicamente una política que ya hayas validado físicamente.
+    loco_policy: G1AmoPolicyCfg = G1AmoPolicyCfg(freq = 50) # la frecuencia se puede eliminar
+
+    mimic_policies: list[G1BeyondMimicPolicyCfg] = [
+        G1BeyondMimicPolicyCfg(
+            policy_name=dance_name,
+
+            ############
+            freq=50,
+            motion_fps=60.0,
+            playback_rate=1.0,
+
+            # Suavizado moderado.
+            action_beta=0.8,
+            ############
+
+            without_state_estimator=False,
+
+            override_robot_anchor_pos=False,
+
+            use_motion_from_model=True,
+
+            # Usa este campo solamente si el modelo no reporta
+            # correctamente el final del movimiento.
+            max_timestep=get_motion_frames(dance_name)
+        ),
+    ]
+
+    do_safety_check: bool = True
+
+@cfg_registry.register
+class g1_hype_real(G1RlLocoMimicPipelineCfg):
+    """
+    B_DadDance con política de locomoción de respaldo.
+
+    Flujo:
+    1. Inicia usando locomoción estable.
+    2. START o R inicia el baile.
+    3. Al terminar el movimiento, vuelve automáticamente a locomoción.
+    4. El proceso continúa ejecutándose y mantiene al robot de pie.
+    """
+
+    dance_name: str = "J_Dance5_Hype"
+
+    robot: str = "g1"
+
+    env: G1RealEnvCfg = G1RealEnvCfg(
+        env_type="UnitreeCppEnv",
+
+        update_with_fk=True,
+        born_place_align=True,
+
+        unitree=G1UnitreeCfg(
+            net_if="eth0",
+        ),
+    )
+
+    ctrl: list[UnitreeCtrlCfg | KeyboardCtrlCfg] = [
+        UnitreeCtrlCfg(
+            combination_init_buttons=[],
+
+            triggers={
+                "A": "[SHUTDOWN]",
+                "Select": "[POLICY_LOCO]",
+                "Start": "[POLICY_MIMIC]",
+            },
+        ),
+
+        KeyboardCtrlCfg(
+            triggers={
+                "r": "[POLICY_MIMIC]",
+                "l": "[POLICY_LOCO]",
+                "o": "[SHUTDOWN]",
+            },
+        ),
+    ]
+
+    # Política que se encarga de mantener equilibrio y locomoción.
+    # Usa únicamente una política que ya hayas validado físicamente.
+    loco_policy: G1AmoPolicyCfg = G1AmoPolicyCfg(freq = 50) # la frecuencia se puede eliminar
+
+    mimic_policies: list[G1BeyondMimicPolicyCfg] = [
+        G1BeyondMimicPolicyCfg(
+            policy_name=dance_name,
+
+            ############
+            freq=50,
+            motion_fps=60.0,
+            playback_rate=1.0,
+
+            # Suavizado moderado.
+            action_beta=0.8,
+            ############
+
+            without_state_estimator=False,
+
+            override_robot_anchor_pos=False,
+
+            use_motion_from_model=True,
+
+            # Usa este campo solamente si el modelo no reporta
+            # correctamente el final del movimiento.
+            max_timestep=get_motion_frames(dance_name)
+        ),
+    ]
+
+    do_safety_check: bool = True
+
+@cfg_registry.register
+class g1_party_real(G1RlLocoMimicPipelineCfg):
+    """
+    Flujo:
+    1. Inicia usando locomoción estable.
+    2. START o R inicia el baile.
+    3. Al terminar el movimiento, vuelve automáticamente a locomoción.
+    4. El proceso continúa ejecutándose y mantiene al robot de pie.
+    """
+
+    dance_name: str = "J_Dance7_Party"
+
+    robot: str = "g1"
+
+    env: G1RealEnvCfg = G1RealEnvCfg(
+        env_type="UnitreeCppEnv",
+
+        update_with_fk=True,
+        born_place_align=True,
+
+        unitree=G1UnitreeCfg(
+            net_if="eth0",
+        ),
+    )
+
+    ctrl: list[UnitreeCtrlCfg | KeyboardCtrlCfg] = [
+        UnitreeCtrlCfg(
+            combination_init_buttons=[],
+
+            triggers={
+                "A": "[SHUTDOWN]",
+                "Select": "[POLICY_LOCO]",
+                "Start": "[POLICY_MIMIC]",
+            },
+        ),
+
+        KeyboardCtrlCfg(
+            triggers={
+                "r": "[POLICY_MIMIC]",
+                "l": "[POLICY_LOCO]",
+                "o": "[SHUTDOWN]",
+            },
+        ),
+    ]
+
+    # Política que se encarga de mantener equilibrio y locomoción.
+    # Usa únicamente una política que ya hayas validado físicamente.
+    loco_policy: G1AmoPolicyCfg = G1AmoPolicyCfg(freq = 50) # la frecuencia se puede eliminar
+
+    mimic_policies: list[G1BeyondMimicPolicyCfg] = [
+        G1BeyondMimicPolicyCfg(
+            policy_name=dance_name,
+
+            ############
+            freq=50,
+            motion_fps=60.0,
+            playback_rate=1.0,
+
+            # Suavizado moderado.
+            action_beta=0.8,
+            ############
+
+            without_state_estimator=False,
+
+            override_robot_anchor_pos=False,
+
+            use_motion_from_model=True,
+
+            # Usa este campo solamente si el modelo no reporta
+            # correctamente el final del movimiento.
+            max_timestep=get_motion_frames(dance_name)
+        ),
+    ]
+
+    do_safety_check: bool = True
+
+
+@cfg_registry.register
+class g1_disco_real(G1RlLocoMimicPipelineCfg):
+    """
+    B_DadDance con política de locomoción de respaldo.
+
+    Flujo:
+    1. Inicia usando locomoción estable.
+    2. START o R inicia el baile.
+    3. Al terminar el movimiento, vuelve automáticamente a locomoción.
+    4. El proceso continúa ejecutándose y mantiene al robot de pie.
+    """
+
+    dance_name: str = "J_ShortDance14_Disco"
+
+    robot: str = "g1"
+
+    env: G1RealEnvCfg = G1RealEnvCfg(
+        env_type="UnitreeCppEnv",
+
+        update_with_fk=True,
+        born_place_align=True,
+
+        unitree=G1UnitreeCfg(
+            net_if="eth0",
+        ),
+    )
+
+    ctrl: list[UnitreeCtrlCfg | KeyboardCtrlCfg] = [
+        UnitreeCtrlCfg(
+            combination_init_buttons=[],
+
+            triggers={
+                "A": "[SHUTDOWN]",
+                "Select": "[POLICY_LOCO]",
+                "Start": "[POLICY_MIMIC]",
+            },
+        ),
+
+        KeyboardCtrlCfg(
+            triggers={
+                "r": "[POLICY_MIMIC]",
+                "l": "[POLICY_LOCO]",
+                "o": "[SHUTDOWN]",
+            },
+        ),
+    ]
+
+    # Política que se encarga de mantener equilibrio y locomoción.
+    # Usa únicamente una política que ya hayas validado físicamente.
+    loco_policy: G1AmoPolicyCfg = G1AmoPolicyCfg(freq = 50) # la frecuencia se puede eliminar
+
+    mimic_policies: list[G1BeyondMimicPolicyCfg] = [
+        G1BeyondMimicPolicyCfg(
+            policy_name=dance_name,
+
+            ############
+            freq=50,
+            motion_fps=60.0,
+            playback_rate=1.0,
+
+            # Suavizado moderado.
+            action_beta=0.8,
+            ############
+
+            without_state_estimator=False,
+
+            override_robot_anchor_pos=False,
+
+            use_motion_from_model=True,
+
+            # Usa este campo solamente si el modelo no reporta
+            # correctamente el final del movimiento.
+            max_timestep=get_motion_frames(dance_name)
+        ),
+    ]
+
+    do_safety_check: bool = True
+
